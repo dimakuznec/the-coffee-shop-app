cc2bb138570f9febea8e42f33dc057e4afc214cb
Dima Kuznecov <kud3544@gmail.com>
2023-03-14 20:12:39 +0300

    git commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function fduh(){
-    console.log('hello');
-}
+let jsContent = document.querySelector('.js-content');
+let jsImgCoffeeCup = document.querySelector('.js-img-coffee-cup');
 
-fduh()
+
+
+document.onmousemove = (e)=>{
+	let x = e.clientX / window.innerWidth;
+	let y = e.clientY / window.innerHeight;
+
+    jsImgCoffeeCup.style.transform = "translate(-" + x * 50 + "px, -" + y * 50 + "px)";
+	jsContent.style.transform = "translate(-" + x * 50 + "px, -" + y * 50 + "px)";
+   
+};

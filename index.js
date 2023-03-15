1ae93215181ca94db039e21b37e97e9f58655211
Dima Kuznecov <kud3544@gmail.com>
2023-03-15 13:24:22 +0300

    git commit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 let jsContent = document.querySelector('.js-content');
 let jsImgCoffeeCup = document.querySelector('.js-img-coffee-cup');
-
-
-
-document.onmousemove = (e)=>{
+// Анимация parallax
+document.onmousemove = (e)=> {
 	let x = e.clientX / window.innerWidth;
 	let y = e.clientY / window.innerHeight;
 
     jsImgCoffeeCup.style.transform = "translate(-" + x * 50 + "px, -" + y * 50 + "px)";
 	jsContent.style.transform = "translate(-" + x * 50 + "px, -" + y * 50 + "px)";
-   
 };
